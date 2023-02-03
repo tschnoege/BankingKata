b42f584599e69a27bce8dc551f139728fa5396eb
tschnoege <thomas.schnoege@codecentric.de>
2023-02-03 17:10:09 +0100

    introducing date and printStatement

--- src/Account.java
+++ src/Account.java
@@ -1,15 +1,65 @@
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Account {
-    private int balance = 0;
+    private final List<AccountMovement> movements = new ArrayList<>();
+
+    public void addAmount(Date valuta, int amount) {
+        movements.add(new AccountMovement(valuta, amount));
+    }
 
     public void deposit(int amount) {
-        balance += amount;
+        this.addAmount(new Date(), amount);
     }
 
     public void withdraw(int amount) {
-        balance -= amount;
+        this.addAmount(new Date(), -amount);
     }
 
     public int getBalance() {
+        int balance = 0;
+
+        for (AccountMovement movement : movements) {
+            balance += movement.getAmount();
+        }
+
         return balance;
+    }
+
+    public List<AccountMovement> getMovements(Date valuta) {
+        assert valuta != null;
+
+        return movements.stream()
+                .filter(accountMovement -> accountMovement.getValuta().equals(valuta))
+                .toList();
+    }
+
+    public List<AccountMovement> getAllMovements() {
+        return movements.stream()
+                .toList();
+    }
+
+    public String printStatement() {
+        int balance = 0;
+        final String pattern = "dd.MM.yyyy";
+        final SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        final StringBuilder statement = new StringBuilder("Date\t\tAmount\tBalance\n");
+
+        for (AccountMovement movement : movements) {
+            final int amount = movement.getAmount();
+
+            balance += amount;
+
+            statement.append(simpleDateFormat.format(movement.getValuta()));
+            statement.append("\t");
+            statement.append(movement.getAmount());
+            statement.append("\t\t");
+            statement.append(balance);
+            statement.append("\n");
+        }
+
+        return statement.toString();
     }
 }
